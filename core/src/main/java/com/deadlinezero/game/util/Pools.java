253e24a70bcfe8e64fdb9e91666b94db674a994c
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-08-12 20:19:41 +0200

    pool player homing missiles

--- core/src/main/java/com/deadlinezero/game/util/Pools.java
+++ core/src/main/java/com/deadlinezero/game/util/Pools.java
@@ -3,27 +3,32 @@ package com.deadlinezero.game.util;
 import com.badlogic.gdx.utils.Array;
 import com.deadlinezero.game.config.GameConfig;
 import com.deadlinezero.game.entities.EnemyProjectile;
+import com.deadlinezero.game.entities.HomingMissile;
 import com.deadlinezero.game.entities.Projectile;
 import com.deadlinezero.game.fx.DamageNumber;
 import com.deadlinezero.game.fx.ImpactFx;
 
 public final class Pools {
     public static final int MAX_HOSTILE_PROJECTILES = 512;
+    public static final int MAX_HOMING_MISSILES = 96;
     public static final int MAX_DAMAGE_NUMBERS = 256;
 
     public final Array<Projectile> projectiles = new Array<>(false, GameConfig.MAX_PROJECTILES);
     public final Array<EnemyProjectile> hostileProjectiles = new Array<>(false, MAX_HOSTILE_PROJECTILES);
+    public final Array<HomingMissile> homingMissiles = new Array<>(false, MAX_HOMING_MISSILES);
     public final Array<ImpactFx> impacts = new Array<>(false, 256);
     public final Array<DamageNumber> damageNumbers = new Array<>(false, MAX_DAMAGE_NUMBERS);
 
     private int projectileCursor;
     private int hostileProjectileCursor;
+    private int homingMissileCursor;
     private int impactCursor;
     private int damageNumberCursor;
 
     public Pools() {
         for (int i = 0; i < GameConfig.MAX_PROJECTILES; i++) projectiles.add(new Projectile());
         for (int i = 0; i < MAX_HOSTILE_PROJECTILES; i++) hostileProjectiles.add(new EnemyProjectile());
+        for (int i = 0; i < MAX_HOMING_MISSILES; i++) homingMissiles.add(new HomingMissile());
         for (int i = 0; i < 256; i++) impacts.add(new ImpactFx());
         for (int i = 0; i < MAX_DAMAGE_NUMBERS; i++) damageNumbers.add(new DamageNumber());
     }
@@ -44,6 +49,16 @@ public final class Pools {
             hostileProjectileCursor = (hostileProjectileCursor + 1) % size;
             EnemyProjectile p = hostileProjectiles.get(hostileProjectileCursor);
             if (!p.active) return p;
+        }
+        return null;
+    }
+
+    public HomingMissile homingMissile() {
+        int size = homingMissiles.size;
+        for (int i = 0; i < size; i++) {
+            homingMissileCursor = (homingMissileCursor + 1) % size;
+            HomingMissile missile = homingMissiles.get(homingMissileCursor);
+            if (!missile.active) return missile;
         }
         return null;
     }
