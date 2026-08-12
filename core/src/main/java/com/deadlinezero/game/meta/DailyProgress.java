3b6e608b64d3b7c02fe171f5935b876ef824ff5d
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-08-12 21:15:23 +0200

    track daily rewarded chest claim

--- core/src/main/java/com/deadlinezero/game/meta/DailyProgress.java
+++ core/src/main/java/com/deadlinezero/game/meta/DailyProgress.java
@@ -5,6 +5,7 @@ public final class DailyProgress {
     public long epochDay = -1L;
     public int loginStreak;
     public boolean loginClaimed;
+    public boolean rewardedChestClaimed;
     public int killsToday;
     public int runsToday;
     public int bossesToday;
@@ -18,6 +19,7 @@ public final class DailyProgress {
         else loginStreak = 1;
         epochDay = day;
         loginClaimed = false;
+        rewardedChestClaimed = false;
         killsToday = 0;
         runsToday = 0;
         bossesToday = 0;
