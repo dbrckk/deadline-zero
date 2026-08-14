373b481361dc8298701c37d71721d4fe803b89c6
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-08-14 10:45:32 +0200

    expose fullscreen ad presentation lifecycle

--- core/src/main/java/com/deadlinezero/game/services/AdsService.java
+++ core/src/main/java/com/deadlinezero/game/services/AdsService.java
@@ -2,7 +2,14 @@ package com.deadlinezero.game.services;
 
 public interface AdsService {
     enum Reward { REVIVE, DOUBLE_LOOT, REROLL_SKILLS, BONUS_CHEST }
+
+    interface FullscreenListener {
+        void onOpening();
+        void onClosed();
+    }
+
     boolean isRewardedReady();
     void showRewarded(Reward reward, Runnable onEarned, Runnable onUnavailable);
     default void preload() {}
+    default void setFullscreenListener(FullscreenListener listener) {}
 }
