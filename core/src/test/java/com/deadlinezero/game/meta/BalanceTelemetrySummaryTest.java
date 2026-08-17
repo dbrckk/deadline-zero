d14444659936690a513acc68f067c12dfa80a689
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-08-17 07:05:21 +0200

    update balance summary samples for mutator field

--- core/src/test/java/com/deadlinezero/game/meta/BalanceTelemetrySummaryTest.java
+++ core/src/test/java/com/deadlinezero/game/meta/BalanceTelemetrySummaryTest.java
@@ -9,9 +9,9 @@ final class BalanceTelemetrySummaryTest {
     @Test void summaryComputesStableBalancingMetrics() {
         Array<BalanceRunSample> samples = new Array<>();
         samples.add(new BalanceRunSample(1, 10, 2, 4, true, 60f, 30,
-            600f, 120f, 80f, 30f, "REDLINE", "REX", "ar9", 2, false));
+            600f, 120f, 80f, 30f, "REDLINE", "STANDARD PRESSURE", "REX", "ar9", 2, false));
         samples.add(new BalanceRunSample(2, 10, 2, 5, false, 120f, 20,
-            600f, 240f, 70f, 40f, "REDLINE", "REX", "ar9", 2, false));
+            600f, 240f, 70f, 40f, "REDLINE", "STANDARD PRESSURE", "REX", "ar9", 2, false));
 
         BalanceTelemetrySummary.Summary summary = BalanceTelemetrySummary.summarize(samples);
         assertEquals(2, summary.runs());
