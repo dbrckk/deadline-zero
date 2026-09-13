1aae3d50be30bdce64e70060a7af1705ef94b10a
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-13 15:19:19 +0200

    Port Play Games cloud save backend onto main

--- android/src/main/java/com/deadlinezero/game/android/AndroidLauncher.java
+++ android/src/main/java/com/deadlinezero/game/android/AndroidLauncher.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.deadlinezero.game.DeadlineZeroGame;
+import com.deadlinezero.game.services.CloudSaveAdapter;
 import com.deadlinezero.game.services.GameServices;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.games.PlayGamesSdk;
 
 public final class AndroidLauncher extends AndroidApplication {
     @Override protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +15,12 @@ public final class AndroidLauncher extends AndroidApplication {
 
         AndroidConsentManager consent = new AndroidConsentManager(this);
         AndroidAdsService ads = new AndroidAdsService(this, consent);
+
+        CloudSaveAdapter cloudSave = CloudSaveAdapter.unavailable();
+        if (BuildConfig.PLAY_GAMES_CONFIGURED) {
+            PlayGamesSdk.initialize(this);
+            cloudSave = new AndroidPlayGamesCloudSaveAdapter(this);
+        }
 
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         config.useImmersiveMode = true;
@@ -27,7 +35,8 @@ public final class AndroidLauncher extends AndroidApplication {
                 new AndroidBillingService(this),
                 new AndroidPrivacyService(this, consent),
                 new AndroidShareService(this),
-                new AndroidHapticsService(this)
+                new AndroidHapticsService(this),
+                cloudSave
             )),
             config
         );
