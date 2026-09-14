936eea2f693c749a2408be05e27b462a8d2140ed
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-14 09:12:54 +0200

    Test frame-rate target navigation

--- core/src/test/java/com/deadlinezero/game/config/GraphicsSettingsTest.java
+++ core/src/test/java/com/deadlinezero/game/config/GraphicsSettingsTest.java
@@ -15,10 +15,22 @@ final class GraphicsSettingsTest {
         }
     }
 
-    @Test void navigationClampsAtEnds() {
+    @Test void qualityNavigationClampsAtEnds() {
         assertEquals(GraphicsSettings.Quality.LOW, GraphicsSettings.Quality.LOW.next(-1));
         assertEquals(GraphicsSettings.Quality.MEDIUM, GraphicsSettings.Quality.LOW.next(1));
         assertEquals(GraphicsSettings.Quality.ULTRA, GraphicsSettings.Quality.ULTRA.next(1));
         assertEquals(GraphicsSettings.Quality.HIGH, GraphicsSettings.Quality.ULTRA.next(-1));
+    }
+
+    @Test void frameRateTargetsAreExplicitAndClampAtEnds() {
+        var values = GraphicsSettings.FrameRate.values();
+        assertEquals(3, values.length);
+        assertEquals(60, values[0].target);
+        assertEquals(90, values[1].target);
+        assertEquals(120, values[2].target);
+        assertEquals(GraphicsSettings.FrameRate.FPS_60, GraphicsSettings.FrameRate.FPS_60.next(-1));
+        assertEquals(GraphicsSettings.FrameRate.FPS_90, GraphicsSettings.FrameRate.FPS_60.next(1));
+        assertEquals(GraphicsSettings.FrameRate.FPS_120, GraphicsSettings.FrameRate.FPS_120.next(1));
+        assertEquals(GraphicsSettings.FrameRate.FPS_90, GraphicsSettings.FrameRate.FPS_120.next(-1));
     }
 }
