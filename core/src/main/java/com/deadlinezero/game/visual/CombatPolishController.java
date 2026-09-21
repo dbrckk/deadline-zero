26290f8f31a51f0cdd6ccaf86cec63e12b42a915
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-21 08:51:10 +0200

    fix(combat): import reaction FX color type

--- core/src/main/java/com/deadlinezero/game/visual/CombatPolishController.java
+++ core/src/main/java/com/deadlinezero/game/visual/CombatPolishController.java
@@ -1,6 +1,7 @@
 package com.deadlinezero.game.visual;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
