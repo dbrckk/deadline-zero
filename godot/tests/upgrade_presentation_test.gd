8785af10ddabe6e2ba1974f896db538f865ee59b
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-26 12:01:11 +0200

    test: fail upgrade presentation checks fast

--- godot/tests/upgrade_presentation_test.gd
+++ godot/tests/upgrade_presentation_test.gd
@@ -1,22 +1,40 @@
 extends SceneTree
 
+func _fail(message: String) -> void:
+    push_error(message)
+    quit(1)
+
 func _init() -> void:
     var main_text := FileAccess.get_file_as_string("res://scripts/Main.gd")
     var hud_text := FileAccess.get_file_as_string("res://scripts/Hud.gd")
 
     var ids := ["damage", "rate", "speed", "health", "projectile", "multishot"]
     for id in ids:
-        assert(main_text.contains("\"id\":\"" + id + "\""))
-        assert(hud_text.contains("\"" + id + "\""))
+        if not main_text.contains("\"id\":\"" + id + "\""):
+            _fail("Upgrade pool is missing id: %s" % id)
+            return
+        if not hud_text.contains("\"" + id + "\""):
+            _fail("HUD presentation is missing id: %s" % id)
+            return
 
-    assert(main_text.contains("\"family\":\"OFFENSE\""))
-    assert(main_text.contains("\"family\":\"SURVIVAL\""))
-    assert(main_text.contains("\"family\":\"BARRAGE\""))
-    assert(hud_text.contains("func _upgrade_glyph"))
-    assert(hud_text.contains("func _upgrade_color"))
-    assert(hud_text.contains("func _style_upgrade_card"))
-    assert(hud_text.contains("StyleBoxFlat.new()"))
-    assert(hud_text.contains("upgrade_family_labels"))
-    assert(hud_text.contains("upgrade_detail_labels"))
+    var required_main := ["\"family\":\"OFFENSE\"", "\"family\":\"SURVIVAL\"", "\"family\":\"BARRAGE\""]
+    for token in required_main:
+        if not main_text.contains(token):
+            _fail("Upgrade presentation is missing family token: %s" % token)
+            return
+
+    var required_hud := [
+        "func _upgrade_glyph",
+        "func _upgrade_color",
+        "func _style_upgrade_card",
+        "StyleBoxFlat.new()",
+        "upgrade_family_labels",
+        "upgrade_detail_labels"
+    ]
+    for token in required_hud:
+        if not hud_text.contains(token):
+            _fail("HUD upgrade presentation is missing token: %s" % token)
+            return
+
     print("godot upgrade presentation test passed")
     quit(0)
